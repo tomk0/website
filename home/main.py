529c861e6e182b9cf41371e1afb9e491b16c2561
tomk0 <tomkelly16111996@gmail.com>
2020-06-03 14:48:45 +0100

    test commit

--- home/main.py
+++ home/main.py
@@ -1,14 +1,20 @@
-import requests
+import requests, json
+import pandas as pd
 
 def main():
-    json = getData("https://c19downloads.azureedge.net/downloads/json/coronavirus-cases_latest.json")
+    rawJSON = getData("https://c19downloads.azureedge.net/downloads/json/coronavirus-cases_latest.json")
 
+    jsonToDataframe(rawJSON)
 
 def getData(loc):
     response = requests.get(loc)
     data = response.json()
 
     return data
+
+def jsonToDataframe(json):
+
+    return 0    
 
 if __name__ == "__main__":
     main()
